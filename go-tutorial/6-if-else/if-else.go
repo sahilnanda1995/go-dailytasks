41e9cb98e8f4eeaf8594b2a90e3e6f4eb571055d
Sahil <sahilnanda1995@gmail.com>
2020-01-25 15:37:05 +0530

    if-else

---
+++ go-tutorial/6-if-else/if-else.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    if 7%2 == 0 {
+        fmt.Println("7 is even")
+    } else {
+        fmt.Println("7 is odd")
+    }
+
+    if 8%4 == 0 {
+        fmt.Println("8 is divisible by 4")
+	}
+	// Note that you don’t need parentheses around conditions in Go, 
+	// but the braces are required.
+
+    if num := 9; num < 0 {
+        fmt.Println(num, "is negative")
+    } else if num < 10 {
+        fmt.Println(num, "has 1 digit")
+    } else {
+        fmt.Println(num, "has multiple digits")
+	}
+	// There is no ternary if in Go, so you’ll need to use a full 
+	// if statement even for basic conditions.
+}
