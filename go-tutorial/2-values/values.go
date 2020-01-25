6a596160d08b88ca25a92033c61559d112cb88e2
Sahil <sahilnanda1995@gmail.com>
2020-01-25 13:39:45 +0530

    values

---
+++ go-tutorial/2-values/values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    fmt.Println("Hakuna " + "Matata")
+
+    fmt.Println("Let's check whether 1+1 is 11 ", 1+1)
+    fmt.Println("float check 7.0/3.0 =", 7.0/3.0)
+
+    fmt.Println("bool check ", true && false)
+    fmt.Println("Life is awesome: ", true || false)
+    fmt.Println("go is difficult", !true)
+}
